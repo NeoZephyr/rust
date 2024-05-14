6a558389910beb8fa2368a717f45d3e86cd7937b
meilb <meimlb@163.com>
2024-05-14 14:12:46 +0800

    feat: type

--- learning/examples/composite.rs
+++ learning/examples/composite.rs
@@ -253,10 +253,13 @@ fn match_case1() {
     let ret = match rect {
         Shape::Rectangle(_) => 1,
         Shape::Triangle(_, _, _) => 2,
-        Shape::Circle { origin: (_, _), radius: _ } => 3,
+        Shape::Circle { origin: (x, y), radius: _ } if x == 0 && y == 0  => 3,
+        Shape::Circle { origin: _, radius: 0 } => 4,
+        Shape::Circle { .. } => 5
     };
 
-    if let Shape::Triangle(_, _, _) = rect {
+    // .. 忽略剩余值
+    if let Shape::Triangle(_, ..) = rect {
         println!("is triangle")
     } else {
         println!("is not triangle")
@@ -358,4 +361,26 @@ fn closure() {
     // fn multiply_v1(x: u32) -> u32 {x * factor}
     let multiply_v2 = |x| {x * factor};
     println!("multiply 12: {}", multiply_v2(12))
+}
+
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String)
+}
+
+fn filter_ipv4() {
+    let ips = vec![
+        IpAddr::V4(String::from("127.0.0.1")),
+        IpAddr::V6(String::from("::1")),
+        IpAddr::V4(String::from("192.168.4.4"))
+    ];
+
+    let filter_ips: Vec<&IpAddr> = ips.iter().filter(|x| matches!(x, IpAddr::v4(_))).collect();
+
+    println!("filter_ips: {:?}", filter_ips);
+
+    for (index, value) in filter_ips.iter().enumerate() {
+        println!("{}, {:?}", index, value)
+    }
 }
--- learning/examples/generic.rs
+++ learning/examples/generic.rs
@@ -1,12 +1,7 @@
 fn main() {
-
-    println!("3 + 5 = {}", add(3, 5));
-    println!("3.14 + 2.78 = {}", add(3.14f64, 2.78f64));
-
-    let p1 = Point{x: 1, y: 2};
-    let p2 = Point{x: 3.14f64, y: 2.78f64};
-    println!("point: {:?}", p1);
-    println!("distance: {}", p2.distance_from_origin());
+    type_case1();
+    type_case2();
+    type_case3();
 
     let arr: [i32; 3] = [1, 2, 3];
     display_array(arr);
@@ -15,28 +10,104 @@ fn main() {
     display_array(arr);
 }
 
-fn add<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
-    a + b
-}
-
-fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
-    println!("{:?}", arr);
-}
-
+// Point<T> 定义类型参数 T
 #[derive(Debug)]
 struct Point<T> {
     x: T,
     y: T
 }
 
+// 变体 V1 带了一个 Point 的负载，
+// 变体 V2 带了一个 Vec 的负载
+enum A<T, U> {
+    V1(Point<T>),
+    V2(Vec<U>)
+}
+
+// Point<T> 使用类型参数 T
 impl<T> Point<T> {
     fn x(&self) -> &T {
         &self.x
     }
 }
 
-impl Point<f64> {
-    fn distance_from_origin(&self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+// 对具化类型做 impl
+impl Point<i32> {
+    fn move_right(&mut self) -> i32 {
+        self.x = self.x + 1;
+        self.x
     }
+}
+
+fn type_case1() {
+    // 推导参数类型
+    let p1 = Point{x: 1, y: 2};
+    let p2 = Point{x: 3.14f64, y: 2.78f64};
+
+    // turbofish 语法提供类型参数信息
+    let p3 = Point::<u8>{x: 1, y: 30};
+
+    // let p3 = Point{x: 1, y: 3.14};
+    println!("p1: {:?}, p2: {:?}, p3: {:?}", p1, p2, p3);
+
+    let mut p4 = Point{x: 5, y: 6};
+    let r = &mut p4;
+    r.move_right();
+    println!("p4: {:?}", p4)
+}
+
+fn print<T: std::fmt::Display>(p: Point<T>) {
+    println!("Point(x -> {}, y -> {})", p.x, p.y)
+}
+
+fn add<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+struct B<T, U> {
+    x: T,
+    y: U
+}
+
+// 使用 4 个类型参数：X1、Y1、X2、Y2
+impl<X1, Y1> B<X1, Y1> {
+    fn mix_up<X2, Y2>(self, other: B<X2, Y2>) -> B<X1, Y2> {
+        B {
+            x: self.x,
+            y: other.y
+        }
+    }
+}
+
+fn type_case2() {
+    let p = Point{x: 10, y: 20};
+    print(p);
+
+    println!("3 + 5 = {}", add(3, 5));
+    println!("3.14 + 2.78 = {}", add(3.14f64, 2.78f64));
+}
+
+// newtype 模式
+// 用新的类型名字替换里面原来那个类型名字
+#[derive(Debug)]
+struct List<T>(Vec<T>);
+// struct List(Vec<u8>);
+
+use std::collections::HashMap;
+struct AAA(Vec<u8>);
+struct BBB {
+    map: HashMap<String, AAA>
+}
+type CCC = HashMap<String, Vec<BBB>>;
+
+// 洋葱类型重命名
+type MyType<T, U> = HashMap<U, Vec<HashMap<String, Vec<HashMap<String, Vec<T>>>>>>;
+
+fn type_case3() {
+    let list = List::<i32>(vec![1, 2, 3]);
+    println!("list: {:?}", list)
+}
+
+fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("{:?}", arr);
 }
